c3a11161d36742a78d4dbbb6937e3cf6d4201370
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-08 03:52:51 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,21 @@
+# class Person
+#   def initialize (persons_name)
+#     @name = persons_name
+#   end
+
+#   def name=(persons_name)
+#     @name = persons_name
+#   end
+
+#   def name
+#     @name
+#   end
+#   end
+#this can also be set as 
 class Person
-  def initialize (persons_name)
+  def initialize(persons_name)
     @name = persons_name
   end
+end
 
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-  end
+#because when you  use initialize there is  no need to do the extra coding
